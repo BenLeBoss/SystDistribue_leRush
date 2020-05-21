8bfdf9077120a9dee40239d4995b5ff41a74637f
BenLeBoss <49184923+BenLeBoss@users.noreply.github.com>
2020-05-21 23:08:09 +0200

    Fini normalement, sauf erreur par la suite

--- infos.c
+++ infos.c
@@ -1,15 +1,54 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #include "infos.h"
 
-void set_taille(tableau_entiers_t tab,int t)
+void set_taille(tableau_entiers_t *tab,int t)
 {
-  tab.taille = t;
+  tab->taille = t;
 }
 
-void set_tableau(tableau_entiers_t tab)
+
+void set_tableau(tableau_entiers_t *tab)
 {
-  for (int i=0; i<=tab.taille; i++){
-    tab.tableau[i] = i;
+  //tab->tableau = (int*)malloc(2*sizeof(int));
+  for (int i=0; i<=tab->taille; i++){
+    tab->tableau[i] = i;
   }
 }
+
+
+void set_port(configuration_t *config)
+{
+  config->port = 4000;
+}
+
+
+int set_hostname(configuration_t *config)
+{
+  char host[50];
+  int val;
+  val = gethostname(host, sizeof(host));
+  if (val == -1){
+    perror("Unable to get the hostname");
+    return -1;
+  }
+  printf("hostname : %s\n", host);
+  config->hostname = host;
+  printf("hostname de la structure : %s\n\n", config->hostname);
+  return 0;
+}
+
+// int main(){
+//   configuration_t config;
+//   set_port(&config);
+//   set_hostname(&config);
+//
+//   tableau_entiers_t tab;
+//   set_taille(&tab,5);
+//   set_tableau(&tab);
+//
+//   printf("port du main : %d\nhostname du main : %s\n",config.port, config.hostname);
+//   return 0;
+// }
--- infos.h
+++ infos.h
@@ -7,11 +7,21 @@
 typedef struct
 {
   int taille;
-  int tableau[];
+  int tableau[100];
 }tableau_entiers_t;
 
-void set_taille(tableau_entiers_t tab,int t);
+typedef struct
+{
+  int port;
+  char *hostname;
+}configuration_t;
 
-void set_tableau(tableau_entiers_t tab);
+void set_taille(tableau_entiers_t *tab,int t);
+
+void set_tableau(tableau_entiers_t *tab);
+
+void set_port(configuration_t *config);
+
+int set_hostname(configuration_t *config);
 
 #endif
