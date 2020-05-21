1c14af17cf7a2215ba00cf911f5d6c344ae24d7b
BenLeBoss <49184923+BenLeBoss@users.noreply.github.com>
2020-05-21 18:27:42 +0200

    Add files via upload

--- client.c
+++ client.c
@@ -18,22 +18,35 @@
 int main(void)
 {
 
+  //déclaration de la socket
+  struct sockaddr_in addr_server;
+  int socket = socket(AF_INET, SOCK_DGRAM, 0);
+  if (socket == -1){
+    error("socket error\n");
+    return -1
+  }
+
+  //récupération de l'identifiant du serveur
+  char hostname[50];
+  int val;
+  val = gethostname(hostname, sizeof(hostname));
+  if (val == -1){
+    perror("Unable to get the hostname");
+    return -1;
+  }
+
+  struct hostent *serveur_host;
+  serveur_host = gethostbyname(hostname);
 
 
-  int socketCl = socket(AF_INET, SOCK_STREAM, 0);
-  struct sockaddr_in addrCl;
-  addrCl.sin_addr.s_addr = inet_addr("127.0.1.1");
-  addrCl.sin_family = AF_INET;
-  addrCl.sin_port = htons(30000);
+  //création de la socket du serveur
+  bzero(&addr_server, sizeof(struct sockaddr_in));
+  addr_server.sin_family = AF_INET;
+  addr_server.sin_port = htons(4000);
+  memcpy(&addr_cserver.sin_addr.s_addr, serveur_host->h_addr, serveur_host->h_length);
 
-  connect(socketCl, (const struct sockaddr *)&addrCl, sizeof(addrCl));
-  printf("connecté\n");
 
-  int valeur = 0;
-  recv(socketCl, &valeur, sizeof(int), 0);
-  printf("La valeur reçue est : %d\n", valeur);
-
-  close(socketCl);
+  close(socket);
 
   return 0;
 }
